e917b26d7334ac93e57ee38ff21462eb4fdede16
cfuta-ia <cfuta@inductiveautomation.com>
2023-01-13 15:43:57 -0700

    commit

--- framework/tiler/splitter.py
+++ framework/tiler/splitter.py
@@ -1,5 +1,14 @@
 from PIL import Image
+import numpy as np
 class ImageSplitter:
     """Image Spliiter Class for splitting an image into tiles"""
-    def __init__(self, image):
-        self.image = image
+    def __init__(self, imagePath, zoom=1):
+        self.image = Image.open(imagePath)
+        self.image_data = np.asarray(self.image)
+        self.zoom = zoom
+
+    @property
+    def imageSize(self):
+        """Image Size Property"""
+        print(self.image_data.shape)
+        return self.image_data.shape
